b96bf3ebf4df7d250995f50909083532d019d061
Carlos Andres Chapid Inga <carloschapid@unicauca.edu.co>
2019-11-04 15:03:19 -0500

    Mas inserts

--- enum.sql
+++ enum.sql
@@ -10,13 +10,25 @@ CREATE TYPE ofertas.dominio_nivel_software_oferta AS ENUM ('Ninguno', 'Nivel baj
 CREATE TYPE ofertas.dominio_tipo_licencia_oferta AS ENUM ('A1', 'A2', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3');
 CREATE TYPE ofertas.dominio_estado_postulacion_oferta AS ENUM ('Contratado', 'Muy bueno', 'Bueno', 'Descartado', 'Desistió', 'Sin clasificación');
 
-commit;
 
+--Egresados Dominios
+CREATE TYPE ofertas.dominio_estado_solicitud_carnetizacion AS ENUM ('Pendiente', 'Realizado');
+CREATE TYPE ofertas.dominio_genero_egresado AS ENUM ('Masculino', 'Femenino');
+CREATE TYPE ofertas.dominio_estado_egresado AS ENUM ('En espera', 'Activo', 'Inactivo');
+CREATE TYPE ofertas.dominio_estado_civil_egresado AS ENUM ('Soltero', 'Casado', 'Union Libre');
+CREATE TYPE ofertas.dominio_grupo_etnico_egresado AS ENUM ('Mestizo', 'Caucasico', 'Afrocolombiano', 'Indigena', 'Otro grupo');
+
+
+commit;
+--
 ALTER TABLE ofertas.contratos ALTER COLUMN tipo_contrato TYPE ofertas.dominio_tipo_contrato USING tipo_contrato::ofertas.dominio_tipo_contrato;
 
 ALTER TABLE ofertas.empresas ALTER COLUMN estado TYPE ofertas.dominio_estado_empresa USING estado::ofertas.dominio_estado_empresa;
 
 ALTER TABLE ofertas.ofertas ALTER COLUMN experiencia TYPE ofertas.dominio_experiencia_oferta USING experiencia::ofertas.dominio_experiencia_oferta;
+
+ALTER TABLE ofertas.salarios ALTER COLUMN forma_pago TYPE ofertas.dominio_contrato_forma_pago USING forma_pago::ofertas.dominio_contrato_forma_pago;
+
 
 ALTER TABLE ofertas.ofertas ALTER COLUMN estado TYPE ofertas.dominio_estado_oferta USING estado::ofertas.dominio_estado_oferta;
 
@@ -31,3 +43,23 @@ ALTER TABLE ofertas.ofertas_software ALTER COLUMN nivel TYPE ofertas.dominio_niv
 ALTER TABLE ofertas.ofertas ALTER COLUMN licencia_conduccion TYPE ofertas.dominio_tipo_licencia_oferta USING licencia_conduccion::ofertas.dominio_tipo_licencia_oferta;
 
 ALTER TABLE ofertas.postulaciones ALTER COLUMN estado TYPE ofertas.dominio_estado_postulacion_oferta USING estado::ofertas.dominio_estado_postulacion_oferta;
+
+alter table ofertas.contratos alter column jornada_laboral type ofertas.dominio_jornada_laboral using jornada_laboral::ofertas.dominio_jornada_laboral;
+
+
+--Egresados ALTER TABLE
+ALTER TABLE ofertas.carnetizacion ALTER COLUMN estado_solicitud TYPE ofertas.dominio_estado_solicitud_carnetizacion USING estado_solicitud::ofertas.dominio_estado_solicitud_carnetizacion;
+ALTER TABLE ofertas.egresados ALTER COLUMN genero TYPE ofertas.dominio_genero_egresado USING genero::ofertas.dominio_genero_egresado;
+ALTER TABLE ofertas.egresados ALTER COLUMN estado TYPE ofertas.dominio_estado_egresado USING estado::ofertas.dominio_estado_egresado;
+ALTER TABLE ofertas.egresados ALTER COLUMN estado_civil TYPE ofertas.dominio_estado_civil_egresado USING estado_civil::ofertas.dominio_estado_civil_egresado;
+ALTER TABLE ofertas.egresados ALTER COLUMN grupo_etnico TYPE ofertas.dominio_grupo_etnico_egresado USING grupo_etnico::ofertas.dominio_grupo_etnico_egresado;
+
+
+INSERT INTO ofertas.discapacidades( id_aut_discapacidades, nombre ) VALUES ( 1, 'Problemas neurologicos' ); 
+INSERT INTO ofertas.idiomas( id_aut_idioma, nombre ) VALUES ( 1, 'Español' ); 
+INSERT INTO ofertas.idiomas( id_aut_idioma, nombre ) VALUES ( 2, 'Ingles' ); 
+INSERT INTO ofertas.idiomas( id_aut_idioma, nombre ) VALUES ( 3, 'Italiano' ); 
+INSERT INTO ofertas.idiomas( id_aut_idioma, nombre ) VALUES ( 4, 'Mandarin' ); 
+INSERT INTO ofertas.sectores( id_aut_sector, nombre ) VALUES ( 1, 'Salud' ); 
+INSERT INTO ofertas.programas( id_aut_programa, nombre, id_facultad, id_sede ) VALUES ( 1, 'Sistemas', null, null ); 
+INSERT INTO ofertas.programas( id_aut_programa, nombre, id_facultad, id_sede ) VALUES ( 2, 'Fisica', null, null ); 
