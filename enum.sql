d3e0333fcbc01ddace28354c42ab85069e4c1a32
Carlos Andres Chapid Inga <carloschapid@unicauca.edu.co>
2019-11-05 11:29:46 -0500

    Campo descripcion y fecha vencimiento

--- enum.sql
+++ enum.sql
@@ -56,10 +56,14 @@ ALTER TABLE ofertas.egresados ALTER COLUMN grupo_etnico TYPE ofertas.dominio_gru
 
 
 INSERT INTO ofertas.discapacidades( id_aut_discapacidades, nombre ) VALUES ( 1, 'Problemas neurologicos' ); 
-INSERT INTO ofertas.idiomas( id_aut_idioma, nombre ) VALUES ( 1, 'Español' ); 
-INSERT INTO ofertas.idiomas( id_aut_idioma, nombre ) VALUES ( 2, 'Ingles' ); 
-INSERT INTO ofertas.idiomas( id_aut_idioma, nombre ) VALUES ( 3, 'Italiano' ); 
-INSERT INTO ofertas.idiomas( id_aut_idioma, nombre ) VALUES ( 4, 'Mandarin' ); 
+
 INSERT INTO ofertas.sectores( id_aut_sector, nombre ) VALUES ( 1, 'Salud' ); 
+
+INSERT INTO ofertas.sub_sectores(id_aut_sub_sector, nombre, id_sectores ) VALUES ( 1, 'Salud', 1 ); 
+
 INSERT INTO ofertas.programas( id_aut_programa, nombre, id_facultad, id_sede ) VALUES ( 1, 'Sistemas', null, null ); 
 INSERT INTO ofertas.programas( id_aut_programa, nombre, id_facultad, id_sede ) VALUES ( 2, 'Fisica', null, null ); 
+
+insert into ofertas.roles (nombre) values ('Administrador');
+insert into ofertas.roles (nombre) values ('Empresa');
+insert into ofertas.roles (nombre) values ('Egresado');
