fd927483d234ee5063d42740b86a836fabb224ba
Carlos Andres Chapid Inga <carloschapid@unicauca.edu.co>
2019-11-05 12:02:31 -0500

    Actualizacion de Scripts

--- enum.sql
+++ enum.sql
@@ -57,9 +57,9 @@ ALTER TABLE ofertas.egresados ALTER COLUMN grupo_etnico TYPE ofertas.dominio_gru
 
 INSERT INTO ofertas.discapacidades( id_aut_discapacidades, nombre ) VALUES ( 1, 'Problemas neurologicos' ); 
 
-INSERT INTO ofertas.sectores( id_aut_sector, nombre ) VALUES ( 1, 'Salud' ); 
+-- INSERT INTO ofertas.sectores( id_aut_sector, nombre ) VALUES ( 1, 'Salud' ); 
 
-INSERT INTO ofertas.sub_sectores(id_aut_sub_sector, nombre, id_sectores ) VALUES ( 1, 'Salud', 1 ); 
+-- INSERT INTO ofertas.sub_sectores(id_aut_sub_sector, nombre, id_sectores ) VALUES ( 1, 'Salud', 1 ); 
 
 INSERT INTO ofertas.programas( id_aut_programa, nombre, id_facultad, id_sede ) VALUES ( 1, 'Sistemas', null, null ); 
 INSERT INTO ofertas.programas( id_aut_programa, nombre, id_facultad, id_sede ) VALUES ( 2, 'Fisica', null, null ); 
