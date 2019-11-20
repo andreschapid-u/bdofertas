0dec99760af1309990b34f31a24d3c38dc4c168c
CarlosAndres09 <32658740+CarlosAndres09@users.noreply.github.com>
2019-11-20 16:34:48 -0500

    Se agregaron algunos valores a la tabla nivel de estudio

--- 14_Niveles_Estudio.sql
+++ 14_Niveles_Estudio.sql
@@ -1,8 +1,8 @@
-INSERT INTO  ofertas.niveles_estudio(id_aut_estudio,nombre) VALUES (1,'Primaria',false);
-INSERT INTO  ofertas.niveles_estudio(id_aut_estudio,nombre) VALUES (2,'Bachiller',false);
-INSERT INTO  ofertas.niveles_estudio(id_aut_estudio,nombre) VALUES (3,'Técnico',false);
-INSERT INTO  ofertas.niveles_estudio(id_aut_estudio,nombre) VALUES (4,'Tecnologo',false);
-INSERT INTO  ofertas.niveles_estudio(id_aut_estudio,nombre) VALUES (5,'Profesional', true);
-INSERT INTO  ofertas.niveles_estudio(id_aut_estudio,nombre) VALUES (6,'Especialización', true);
-INSERT INTO  ofertas.niveles_estudio(id_aut_estudio,nombre) VALUES (7,'Maestria',true);
-INSERT INTO  ofertas.niveles_estudio(id_aut_estudio,nombre) VALUES (8,'Doctorado',true);
+INSERT INTO  ofertas.niveles_estudio(id_aut_estudio,nombre,pertenece_u) VALUES (1,'Primaria',false);
+INSERT INTO  ofertas.niveles_estudio(id_aut_estudio,nombre,pertenece_u) VALUES (2,'Bachillerato',false);
+INSERT INTO  ofertas.niveles_estudio(id_aut_estudio,nombre,pertenece_u) VALUES (3,'Técnico',false);
+INSERT INTO  ofertas.niveles_estudio(id_aut_estudio,nombre,pertenece_u) VALUES (4,'Tecnología',false);
+INSERT INTO  ofertas.niveles_estudio(id_aut_estudio,nombre,pertenece_u) VALUES (5,'Pregrado', true);
+INSERT INTO  ofertas.niveles_estudio(id_aut_estudio,nombre,pertenece_u) VALUES (6,'Especialización', true);
+INSERT INTO  ofertas.niveles_estudio(id_aut_estudio,nombre,pertenece_u) VALUES (7,'Maestria',true);
+INSERT INTO  ofertas.niveles_estudio(id_aut_estudio,nombre,pertenece_u) VALUES (8,'Doctorado',true);
