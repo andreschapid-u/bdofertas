ef8992cefe68fa586b3ce954b887429e32ff4294
CarlosAndres09 <32658740+CarlosAndres09@users.noreply.github.com>
2019-11-26 10:25:47 -0500

    se agrego la tabla discapacidades

---
+++ 12_Dispacidades.sql
@@ -0,0 +1,7 @@
+INSERT INTO  ofertas.discapacidades(id_aut_discapacidades,nombre) VALUES (1,'No');
+INSERT INTO  ofertas.discapacidades(id_aut_discapacidades,nombre) VALUES (2,'Visual');
+INSERT INTO  ofertas.discapacidades(id_aut_discapacidades,nombre) VALUES (3,'Física');
+INSERT INTO  ofertas.discapacidades(id_aut_discapacidades,nombre) VALUES (4,'Auditiva');
+INSERT INTO  ofertas.discapacidades(id_aut_discapacidades,nombre) VALUES (4,'Fonètica');
+INSERT INTO  ofertas.discapacidades(id_aut_discapacidades,nombre) VALUES (4,'Cognitiva');
+INSERT INTO  ofertas.discapacidades(id_aut_discapacidades,nombre) VALUES (4,'Otra(s)');
