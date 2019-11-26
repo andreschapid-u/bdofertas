ebe64c0d1fd8f0fe565d701b0b94054767091f61
CarlosAndres09 <32658740+CarlosAndres09@users.noreply.github.com>
2019-11-26 12:01:56 -0500

    Update 15_Titulo.sql

--- 15_Titulo.sql
+++ 15_Titulo.sql
@@ -1,3 +1,3 @@
 INSERT INTO  ofertas.titulo(id_aut_titulo,nombre) VALUES (1,'Maestro en violin');
-INSERT INTO  ofertas.titulo(id_aut_titulo,nombre) VALUES (1,'Maestro en percusión');
+INSERT INTO  ofertas.titulo(id_aut_titulo,nombre) VALUES (2,'Maestro en percusión');
  
