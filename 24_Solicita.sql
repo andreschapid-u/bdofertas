fa288cdad898bfd8af9d051c54ac32477f453c14
CarlosAndres09 <32658740+CarlosAndres09@users.noreply.github.com>
2019-12-03 02:07:04 -0500

    Actualizado

---
+++ 24_Solicita.sql
@@ -0,0 +1,4 @@
+--Solicita
+INSERT INTO  ofertas.solicita(id_aut_carnetizacion, id_egresado, fecha_solicitud,fecha_respuesta) VALUES (1,1,'12/11/1995','24/11/1995');
+INSERT INTO  ofertas.solicita(id_aut_carnetizacion, id_egresado, fecha_solicitud,fecha_respuesta) VALUES (2,2,'12/11/1995','24/11/1995');
+INSERT INTO  ofertas.solicita(id_aut_carnetizacion, id_egresado, fecha_solicitud,fecha_respuesta) VALUES (3,3,'12/11/1995','24/11/1995');
