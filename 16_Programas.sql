6be757a316f72c8afdab83e6529c493a19a9f91f
CarlosAndres09 <32658740+CarlosAndres09@users.noreply.github.com>
2019-12-07 16:16:56 -0500

    Actualizado pruebas

--- 16_Programas.sql
+++ 16_Programas.sql
@@ -1,10 +1,10 @@
 -- /*SEDE POPAYAN */
 -- 	Facultad de Ciencias Contables, Económicas y Administrativas.
 -- Tecnologia
-INSERT INTO  ofertas.programas(id_aut_programa,nombre,id_facultad,id_nivelestudio,id_universidad) VALUES (1,'Administración Financiera',1,4,1);
+INSERT INTO  ofertas.programas(id_aut_programa,nombre,id_facultad,id_nivelestudio) VALUES (1,'Administración Financiera',1,4);
 -- Pregrado
-INSERT INTO  ofertas.programas(id_aut_programa,nombre,id_facultad,id_nivelestudio,id_universidad) VALUES (2,'Administración de Empresas',1,5,1);
-INSERT INTO  ofertas.programas(id_aut_programa,nombre,id_facultad,id_nivelestudio,id_universidad) VALUES (3,'Contaduría Pública',1,5,1);
-INSERT INTO  ofertas.programas(id_aut_programa,nombre,id_facultad,id_nivelestudio,id_universidad) VALUES (4,'Turismo',1,5,1);
-INSERT INTO  ofertas.programas(id_aut_programa,nombre,id_facultad,id_nivelestudio,id_universidad) VALUES (5,'Economía',1,5,1);
+INSERT INTO  ofertas.programas(id_aut_programa,nombre,id_facultad,id_nivelestudio) VALUES (2,'Administración de Empresas',1,5);
+INSERT INTO  ofertas.programas(id_aut_programa,nombre,id_facultad,id_nivelestudio) VALUES (3,'Contaduría Pública',1,5);
+INSERT INTO  ofertas.programas(id_aut_programa,nombre,id_facultad,id_nivelestudio,id_titulo) VALUES (4,'Turismo',1,5,1);
+INSERT INTO  ofertas.programas(id_aut_programa,nombre,id_facultad,id_nivelestudio,id_titulo) VALUES (5,'Economía',1,5,2);
 
--- 27_SedeProgramas.sql
+++ 27_SedeProgramas.sql
@@ -1,2 +1,4 @@
 INSERT INTO  ofertas.sede_programas(id_sede, id_programa) VALUES (1,1);
 INSERT INTO  ofertas.sede_programas(id_sede, id_programa) VALUES (1,2);
+INSERT INTO  ofertas.sede_programas(id_sede, id_programa) VALUES (1,3);
+INSERT INTO  ofertas.sede_programas(id_sede, id_programa) VALUES (1,4);
