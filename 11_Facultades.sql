9dddaa1c3c1b7753981d8276c904dadb1b8e6c18
CarlosAndres09 <32658740+CarlosAndres09@users.noreply.github.com>
2019-12-14 18:18:54 -0500

    Actualizado

--- 11_Facultades.sql
+++ 11_Facultades.sql
@@ -1,11 +1,11 @@
-INSERT INTO  ofertas.facultades(id_aut_facultad,nombre,id_universidad) VALUES (1,'Facultad de Ciencias Contables, Económicas y Administrativas');
-INSERT INTO  ofertas.facultades(id_aut_facultad,nombre,id_universidad) VALUES (2,'Facultad de Ingeniería Electrónica y Telecomunicaciones');
-INSERT INTO  ofertas.facultades(id_aut_facultad,nombre,id_universidad) VALUES (3,'Facultad de Derecho, Ciencias Políticas y Sociales');
-INSERT INTO  ofertas.facultades(id_aut_facultad,nombre,id_universidad) VALUES (4,'Facultad de Ingeniería Civil');
-INSERT INTO  ofertas.facultades(id_aut_facultad,nombre,id_universidad) VALUES (5,'Facultad de Ciencias Agrarias');
-INSERT INTO  ofertas.facultades(id_aut_facultad,nombre,id_universidad) VALUES (6,'Facultad de Ciencias de la Salud');
-INSERT INTO  ofertas.facultades(id_aut_facultad,nombre,id_universidad) VALUES (7,'Facultad de Artes');
-INSERT INTO  ofertas.facultades(id_aut_facultad,nombre,id_universidad) VALUES (8,'Facultad de Ciencias Naturales, Exactas y de la Educación');
-INSERT INTO  ofertas.facultades(id_aut_facultad,nombre,id_universidad) VALUES (9,'Facultad de Ciencias Humanas y Sociales');
+INSERT INTO  ofertas.facultades(id_aut_facultad,nombre) VALUES (1,'Facultad de Ciencias Contables, Económicas y Administrativas');
+INSERT INTO  ofertas.facultades(id_aut_facultad,nombre) VALUES (2,'Facultad de Ingeniería Electrónica y Telecomunicaciones');
+INSERT INTO  ofertas.facultades(id_aut_facultad,nombre) VALUES (3,'Facultad de Derecho, Ciencias Políticas y Sociales');
+INSERT INTO  ofertas.facultades(id_aut_facultad,nombre) VALUES (4,'Facultad de Ingeniería Civil');
+INSERT INTO  ofertas.facultades(id_aut_facultad,nombre) VALUES (5,'Facultad de Ciencias Agrarias');
+INSERT INTO  ofertas.facultades(id_aut_facultad,nombre) VALUES (6,'Facultad de Ciencias de la Salud');
+INSERT INTO  ofertas.facultades(id_aut_facultad,nombre) VALUES (7,'Facultad de Artes');
+INSERT INTO  ofertas.facultades(id_aut_facultad,nombre) VALUES (8,'Facultad de Ciencias Naturales, Exactas y de la Educación');
+INSERT INTO  ofertas.facultades(id_aut_facultad,nombre) VALUES (9,'Facultad de Ciencias Humanas y Sociales');
 
 COMMIT;
