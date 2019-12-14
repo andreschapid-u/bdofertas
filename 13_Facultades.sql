c29ed9bb8f402d794c95c4990bd16549e4001afe
CarlosAndres09 <32658740+CarlosAndres09@users.noreply.github.com>
2019-12-14 14:32:20 -0500

    Actualizada

--- 13_Facultades.sql
+++ 13_Facultades.sql
@@ -1,5 +1,5 @@
 INSERT INTO  ofertas.facultades(id_aut_facultad,nombre,id_universidad) VALUES (1,'Facultad de Ciencias Contables, Económicas y Administrativas',1);
-INSERT INTO  ofertas.facultades(id_aut_facultad,nombre,id_universidad) VALUES (2,'Facultad de Ingeniería Electrónica y telecomunicaciones',1);
+INSERT INTO  ofertas.facultades(id_aut_facultad,nombre,id_universidad) VALUES (2,'Facultad de Ingeniería Electrónica y Telecomunicaciones',1);
 INSERT INTO  ofertas.facultades(id_aut_facultad,nombre,id_universidad) VALUES (3,'Facultad de Derecho, Ciencias Políticas y Sociales',1);
 INSERT INTO  ofertas.facultades(id_aut_facultad,nombre,id_universidad) VALUES (4,'Facultad de Ingeniería Civil',1);
 INSERT INTO  ofertas.facultades(id_aut_facultad,nombre,id_universidad) VALUES (5,'Facultad de Ciencias Agrarias',1);
