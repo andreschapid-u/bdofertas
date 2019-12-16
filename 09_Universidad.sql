ddca0f04500f8410fdb183826358536e94f6f70f
CarlosAndres09 <32658740+CarlosAndres09@users.noreply.github.com>
2019-12-16 11:57:24 -0500

    Universidades

--- 09_Universidad.sql
+++ 09_Universidad.sql
@@ -1,5 +1,10 @@
-INSERT INTO  ofertas.universidad(id_aut_universidad,nombre) VALUES (1,'Universidad del Cauca');
-INSERT INTO  ofertas.universidad(id_aut_universidad,nombre) VALUES (2,'Universidad EAN');
-INSERT INTO  ofertas.universidad(id_aut_universidad,nombre) VALUES (3,'Universidad Colegio Mayor de Cundinamarca');
+INSERT INTO  ofertas.universidad(id_aut_universidad,nombre) VALUES (1,'UNIVERSIDAD DEL CAUCA');
+INSERT INTO  ofertas.universidad(id_aut_universidad,nombre) VALUES (2,'UNIVERSIDAD DE NARIÑO');
+INSERT INTO  ofertas.universidad(id_aut_universidad,nombre) VALUES (3,'UNIVERSIDAD SAN BUENAVENTURA');
+INSERT INTO  ofertas.universidad(id_aut_universidad,nombre) VALUES (4,'ESCUELA NAVAL ALMIRANTE PADILLA');
+INSERT INTO  ofertas.universidad(id_aut_universidad,nombre) VALUES (5,'UNIVERSIDAD EAN');
+INSERT INTO  ofertas.universidad(id_aut_universidad,nombre) VALUES (6,'UNIVERSIDAD DEL BOSQUE');
+
+
 
 COMMIT;
