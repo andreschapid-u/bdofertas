a4432c56d6618dc4269c961809610fb79c5cef59
CarlosAndres09 <32658740+CarlosAndres09@users.noreply.github.com>
2019-12-16 11:24:23 -0500

    Actualizados dominios

--- 02_Dominios.sql
+++ 02_Dominios.sql
@@ -22,7 +22,6 @@ CREATE TYPE ofertas.dominio_perfil_oferta AS ENUM ('Profesional', 'Estudiante pr
 CREATE TYPE ofertas.dominio_estado_solicitud_carnetizacion AS ENUM ('Pendiente', 'Respondido','Cancelado');
 CREATE TYPE ofertas.dominio_genero_egresado AS ENUM ('Masculino', 'Femenino');
 CREATE TYPE ofertas.dominio_estado_egresado AS ENUM ('EN ESPERA', 'ACTIVO', 'INACTIVO','PENDIENTE');
-CREATE TYPE ofertas.dominio_estado_carnetizacion AS ENUM ('Aceptado', 'Pendiente','Cancelado');
 CREATE TYPE ofertas.dominio_estado_civil_egresado AS ENUM ('Soltero(a)', 'Casado(a)','Viudo(a)','Union Libre','Separado(a)','Comprometido(a)','Divorciado(a)');
 CREATE TYPE ofertas.dominio_grupo_etnico_egresado AS ENUM ('Afrodescendiente', 'Indígena', 'Mestizo', 'Blanco', 'Otro');
 
@@ -66,7 +65,6 @@ alter table ofertas.empresas alter column ingresos type ofertas.dominio_ingresos
 ALTER TABLE ofertas.carnetizacion ALTER COLUMN estado_solicitud TYPE ofertas.dominio_estado_solicitud_carnetizacion USING estado_solicitud::ofertas.dominio_estado_solicitud_carnetizacion;
 ALTER TABLE ofertas.egresados ALTER COLUMN genero TYPE ofertas.dominio_genero_egresado USING genero::ofertas.dominio_genero_egresado;
 ALTER TABLE ofertas.egresados ALTER COLUMN estado TYPE ofertas.dominio_estado_egresado USING estado::ofertas.dominio_estado_egresado;
--- ALTER TABLE ofertas.egresados ALTER COLUMN estado_solicitud TYPE ofertas.dominio_estado_carnetizacion USING estado_solicitud::ofertas.dominio_estado_carnetizacion;
 ALTER TABLE ofertas.egresados ALTER COLUMN estado_civil TYPE ofertas.dominio_estado_civil_egresado USING estado_civil::ofertas.dominio_estado_civil_egresado;
 ALTER TABLE ofertas.egresados ALTER COLUMN grupo_etnico TYPE ofertas.dominio_grupo_etnico_egresado USING grupo_etnico::ofertas.dominio_grupo_etnico_egresado;
 commit;
