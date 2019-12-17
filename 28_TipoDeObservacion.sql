89c5c0e055349f55726a778530181df04fa29acc
CarlosAndres09 <32658740+CarlosAndres09@users.noreply.github.com>
2019-12-17 17:48:45 -0500

    Se agregaron comentarios

--- 28_TipoDeObservacion.sql
+++ 28_TipoDeObservacion.sql
@@ -1,6 +1,6 @@
 INSERT INTO  ofertas.tipo_de_observacion(id_aut_comentario,pregunta) VALUES (1,'¿Volvería a estudia en la Universidad del Cauca?');
 INSERT INTO  ofertas.tipo_de_observacion(id_aut_comentario,pregunta) VALUES (2,'¿Qué estudiaría?');
-INSERT INTO  ofertas.tipo_de_observacion(id_aut_comentario,pregunta) VALUES (3,'¿Cuál es motivo?');
+INSERT INTO  ofertas.tipo_de_observacion(id_aut_comentario,pregunta) VALUES (3,'¿Cuál es la razón?');
 INSERT INTO  ofertas.tipo_de_observacion(id_aut_comentario,pregunta) VALUES (4,'Comentarios, sugerencias sobre el programa del cual es egresado');
 INSERT INTO  ofertas.tipo_de_observacion(id_aut_comentario,pregunta) VALUES (5,'¿Qué docente del programa considera que influyó positivamente en su formación como profesional?');
 INSERT INTO  ofertas.tipo_de_observacion(id_aut_comentario,pregunta) VALUES (6,'¿Qué espera de la Universidad del Cauca como futuro egresado?');
---
+++ 34_Comenta.sql
@@ -0,0 +1,7 @@
+--Comenta
+INSERT INTO  ofertas.comenta(id_grado,id_comentario,respuesta) VALUES (1,1,'Si');
+INSERT INTO  ofertas.comenta(id_grado,id_comentario,respuesta) VALUES (1,2,'Ingenieria Civil');
+INSERT INTO  ofertas.comenta(id_grado,id_comentario,respuesta) VALUES (1,3,'Me gusta la ciudad de Popayán');
+INSERT INTO  ofertas.comenta(id_grado,id_comentario,respuesta) VALUES (1,4,'Mas infraestructura');
+INSERT INTO  ofertas.comenta(id_grado,id_comentario,respuesta) VALUES (1,5,'Pepito Perez');
+INSERT INTO  ofertas.comenta(id_grado,id_comentario,respuesta) VALUES (1,6,'Mas convenios internacionales');
