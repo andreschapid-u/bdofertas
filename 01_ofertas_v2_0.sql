15b06a71691680160e0517dbeaae27b3391492f9
CarlosAndres09 <32658740+CarlosAndres09@users.noreply.github.com>
2019-12-17 12:13:44 -0500

    Update 01_ofertas_v2_0.sql

--- 01_ofertas_v2_0.sql
+++ 01_ofertas_v2_0.sql
@@ -30,7 +30,7 @@ CREATE  TABLE ofertas.discapacidades (
 CREATE  TABLE ofertas.evento ( 
 	id_aut_evento        serial  NOT NULL ,
 	nombre               varchar(200)  NOT NULL ,
-	fecha_inicio         date   ,
+	fecha_inicio         date  NOT NULL ,
 	fecha_fin            date  NOT NULL ,
 	hora_inicio          date  NOT NULL ,
 	hora_fin             date   ,
