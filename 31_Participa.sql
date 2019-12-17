a103c75964488fbf7c4d16f337dcc67c6f0936a5
CarlosAndres09 <32658740+CarlosAndres09@users.noreply.github.com>
2019-12-17 14:35:51 -0500

    Create 31_Participa.sql

---
+++ 31_Participa.sql
@@ -0,0 +1,4 @@
+--Participa
+INSERT INTO  ofertas.participa(id_egresado,id_evento,como_se_entero) VALUES (1,1,'Radio');
+INSERT INTO  ofertas.participa(id_egresado,id_evento,como_se_entero) VALUES (2,1,'Radio');
+INSERT INTO  ofertas.participa(id_egresado,id_evento,como_se_entero) VALUES (3,2,'Radio');
