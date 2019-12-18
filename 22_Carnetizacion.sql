ab1a005f80b851eed231a84339814dbfa8077d2b
CarlosAndres09 <32658740+CarlosAndres09@users.noreply.github.com>
2019-12-18 01:49:48 -0500

    Update 22_Carnetizacion.sql

--- 22_Carnetizacion.sql
+++ 22_Carnetizacion.sql
@@ -1,6 +1,5 @@
 --Carnetizacion
 INSERT INTO  ofertas.carnetizacion(id_aut_carnetizacion,id_egresado,estado_solicitud,fecha_solicitud,fecha_respuesta) VALUES (1,1,'PENDIENTE','2009/07/12','2009/07/15');
 INSERT INTO  ofertas.carnetizacion(id_aut_carnetizacion,id_egresado,estado_solicitud,fecha_solicitud,fecha_respuesta) VALUES (2,2,'RESPONDIDO','2009/07/12','2009/07/15');
-INSERT INTO  ofertas.carnetizacion(id_aut_carnetizacion,id_egresado,estado_solicitud,fecha_solicitud,fecha_respuesta) VALUES (3,3.'CANCELADO','2009/07/12','2009/07/15');
 
 COMMIT;
