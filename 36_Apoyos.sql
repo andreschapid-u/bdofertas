7804da2ee8d3f1b62784fc0e5e6ae47654a20d67
CarlosAndres09 <32658740+CarlosAndres09@users.noreply.github.com>
2019-12-18 05:16:21 -0500

    Actualizado

---
+++ 36_Apoyos.sql
@@ -0,0 +1,2 @@
+--Apoyos
+INSERT INTO  ofertas.apoyos(id_aut_apoyo,nombre_rol,id_user,nombres,apellidos,correo,correo_secundario) VALUES (1,'Pepito',1,'Maria','Brito','maria@hotmail.com','maria2@hotmail.com');
---
+++ 37_Acceso.sql
@@ -0,0 +1,2 @@
+--Acceso
+INSERT INTO  ofertas.acceso(id_apoyo,id_servicio) VALUES (1,1);
