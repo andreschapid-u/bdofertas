cf8df1bbd00d11e18b031b6e8174e6ea66032a53
CarlosAndres09 <32658740+CarlosAndres09@users.noreply.github.com>
2019-12-18 05:17:27 -0500

    Update 04_Roles.sql

--- 04_Roles.sql
+++ 04_Roles.sql
@@ -2,5 +2,6 @@
 insert into ofertas.rol(id_aut_rol,nombre) values (1,'Administrador');
 insert into ofertas.rol(id_aut_rol,nombre) values (2,'Empresa');
 insert into ofertas.rol(id_aut_rol,nombre) values (3,'Egresado');
+insert into ofertas.rol(id_aut_rol,nombre) values (4,'Apoyo');
 
 commit;
