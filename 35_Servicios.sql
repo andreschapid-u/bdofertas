8f122774629ee5d414898f06b47b27de85b5fb0b
CarlosAndres09 <32658740+CarlosAndres09@users.noreply.github.com>
2019-12-18 05:06:57 -0500

    Create 35_Servicios.sql

---
+++ 35_Servicios.sql
@@ -0,0 +1,5 @@
+--Servicios
+INSERT INTO  ofertas.servicios(id_aut_servicio,nombre) VALUES (1,'Carnetización');
+INSERT INTO  ofertas.servicios(id_aut_servicio,nombre) VALUES (2,'Eventos');
+INSERT INTO  ofertas.servicios(id_aut_servicio,nombre) VALUES (3,'Egresados');
+INSERT INTO  ofertas.servicios(id_aut_servicio,nombre) VALUES (4,'Ofertas');
