a1385c78b76e15b9dc3e9a089ceae46deff3381c
CarlosAndres09 <32658740+CarlosAndres09@users.noreply.github.com>
2020-02-08 16:14:53 -0500

    Update los datos de la tabla tipo de observacion

--- 28_TipoDeObservacion.sql
+++ 28_TipoDeObservacion.sql
@@ -1,6 +1,6 @@
-INSERT INTO  ofertas.tipo_de_observacion(id_aut_comentario,pregunta) VALUES (1,'¿Volvería a estudia en la Universidad del Cauca?');
-INSERT INTO  ofertas.tipo_de_observacion(id_aut_comentario,pregunta) VALUES (2,'¿Qué estudiaría?');
-INSERT INTO  ofertas.tipo_de_observacion(id_aut_comentario,pregunta) VALUES (3,'¿Cuál es la razón?');
-INSERT INTO  ofertas.tipo_de_observacion(id_aut_comentario,pregunta) VALUES (4,'Comentarios, sugerencias sobre el programa del cual es egresado');
-INSERT INTO  ofertas.tipo_de_observacion(id_aut_comentario,pregunta) VALUES (5,'¿Qué docente del programa considera que influyó positivamente en su formación como profesional?');
-INSERT INTO  ofertas.tipo_de_observacion(id_aut_comentario,pregunta) VALUES (6,'¿Qué espera de la Universidad del Cauca como futuro egresado?');
+INSERT INTO  ofertas.tipo_de_observacion(id_aut_comentario,pregunta,pregunta_padre) VALUES (1,'¿Volvería a estudia en la Universidad del Cauca?',null);
+INSERT INTO  ofertas.tipo_de_observacion(id_aut_comentario,pregunta,pregunta_padre) VALUES (2,'¿Qué estudiaría?',1);
+INSERT INTO  ofertas.tipo_de_observacion(id_aut_comentario,pregunta,pregunta_padre) VALUES (3,'¿Cuál es la razón?',1);
+INSERT INTO  ofertas.tipo_de_observacion(id_aut_comentario,pregunta,pregunta_padre) VALUES (4,'Comentarios, sugerencias sobre el programa del cual es egresado',null);
+INSERT INTO  ofertas.tipo_de_observacion(id_aut_comentario,pregunta,pregunta_padre) VALUES (5,'¿Qué docente del programa considera que influyó positivamente en su formación como profesional?',null);
+INSERT INTO  ofertas.tipo_de_observacion(id_aut_comentario,pregunta,pregunta_padre) VALUES (6,'¿Qué espera de la Universidad del Cauca como futuro egresado?',null);
