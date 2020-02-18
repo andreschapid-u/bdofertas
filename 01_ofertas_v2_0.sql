894e728e9526117f8e46b41308c42117b6403cce
CarlosAndres09 <32658740+CarlosAndres09@users.noreply.github.com>
2020-02-18 08:29:51 -0500

    Unique identificación egresado

--- 01_ofertas_v2_0.sql
+++ 01_ofertas_v2_0.sql
@@ -201,7 +201,8 @@ CREATE  TABLE ofertas.egresados (
 	estado_completar     bool DEFAULT false NOT NULL ,
 	created_at           timestamp DEFAULT current_timestamp NOT NULL ,
 	updated_at           timestamp DEFAULT current_timestamp NOT NULL ,
-	CONSTRAINT pk_egresados_id PRIMARY KEY ( id_aut_egresado )
+	CONSTRAINT pk_egresados_id PRIMARY KEY ( id_aut_egresado ),
+	CONSTRAINT idx_egresados_identificacion UNIQUE ( identificacion ) 
  );
 
 CREATE  TABLE ofertas.egresados_discapacidades ( 
@@ -611,4 +612,3 @@ ALTER TABLE ofertas.ubicacion_oferta ADD CONSTRAINT fk_ubicacion_oferta_ofertas 
 ALTER TABLE ofertas.ubicacion_oferta ADD CONSTRAINT fk_ubicacion_oferta_ciudades FOREIGN KEY ( id_ciudad ) REFERENCES ofertas.ciudades( id_aut_ciudad );
 
 ALTER TABLE ofertas.users ADD CONSTRAINT fk_users_roles FOREIGN KEY ( id_rol ) REFERENCES ofertas.rol( id_aut_rol );
-
